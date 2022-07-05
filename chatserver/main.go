2a24b37c5ce849aa13b4e7827d3ea6d9976d6893
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2022-07-05 09:04:06 +0430

    server source code

---
+++ chatserver/main.go
@@ -0,0 +1,220 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"sync"
+)
+
+// message sending
+
+// SendCommand is used for sending new message from client
+
+type SendCommand struct {
+	Message string
+}
+
+// NameCommand is used for setting client display name
+type NameCommand struct {
+	Name string
+}
+
+// MessageCommand is used for notifying new messages
+type MessageCommand struct {
+	Name    string
+	Message string
+}
+
+//  You can implement a reader to parse the command from the string and write a writer method to convert the command back into a string.
+// The Go programming language uses io.Reader and io.Writer as built-in interfaces,
+// and hence the implementation of these methods is not necessarily aware that these methods are used for a TCP stream.
+
+type CommandWriter struct {
+	writer io.Writer
+}
+
+func NewCommandWriter(writer io.Writer) *CommandWriter {
+	return &CommandWriter{
+		writer: writer,
+	}
+}
+func (w *CommandWriter) writeString(msg string) error {
+	_, err := w.writer.Write([]byte(msg))
+	return err
+}
+func (w *CommandWriter) Write(command interface{}) error {
+	// naive implementation ...
+	var err error
+	switch v := command.(type) {
+	case SendCommand:
+		err = w.writeString(fmt.Sprintf("SEND %v\n", v.Message))
+	case MessageCommand:
+		err = w.writeString(fmt.Sprintf("MESSAGE %v %v\n", v.Name, v.Message))
+	case NameCommand:
+		err = w.writeString(fmt.Sprintf("NAME %v\n", v.Name))
+	default:
+		err = UnknownCommand
+	}
+	return err
+}
+
+// reader method
+// mostly use for error handling
+
+type CommandReader struct {
+	reader *bufio.Reader
+}
+
+func NewCommandReader(reader io.Reader) *CommandReader {
+	return &CommandReader{
+		reader: bufio.NewReader(reader),
+	}
+}
+func (r *CommandReader) Read() (interface{}, error) {
+	// Read the first part
+	commandName, err := r.reader.ReadString(' ')
+	if err != nil {
+		return nil, err
+	}
+	switch commandName {
+	case "MESSAGE ":
+		user, err := r.reader.ReadString(' ')
+		if err != nil {
+			return nil, err
+		}
+		message, err := r.reader.ReadString('\n')
+		if err != nil {
+			return nil, err
+		}
+		return MessageCommand{
+			user[:len(user)-1],
+			message[:len(message)-1],
+		}, nil
+		// similar implementation for other commands
+	default:
+		log.Printf("Unknown command: %v", commandName)
+	}
+	return nil, UnknownCommand
+}
+
+// Setting up the chat server:
+// In this section, the server interface can be defined as follows.
+// I don't know anything about the interface yet and will research it in the future.
+// The interface makes the definition of behaviors clearer.
+
+type ChatServer interface {
+	Listen(address string) error
+	Broadcast(command interface{}) error
+	Start()
+	Close()
+}
+
+// The server listens to incoming connections using the Listen() method, and the Start() and Close() methods are
+//  used to start and stop the server, respectively, and the BroadCast() method is used to send commands to other clients.
+
+// Now we can see the actual implementation of the server.
+
+type TcpChatServer struct {
+	listener net.Listener
+	clients  []*client
+	mutex    *sync.Mutex
+}
+type client struct {
+	conn   net.Conn
+	name   string
+	writer *protocol.CommandWriter
+}
+
+func (s *TcpChatServer) Listen(address string) error {
+	l, err := net.Listen("tcp", address)
+	if err == nil {
+		s.listener = l
+	}
+	log.Printf("Listening on %v", address)
+	return err
+}
+func (s *TcpChatServer) Close() {
+	s.listener.Close()
+}
+func (s *TcpChatServer) Start() {
+	for {
+		// need a way to break the loop
+		conn, err := s.listener.Accept()
+		if err != nil {
+			log.Print(err)
+		} else {
+			// handle connection
+			client := s.accept(conn)
+			go s.serve(client)
+		}
+	}
+}
+
+// When the server accepts a connection, it creates a client structure to keep track of clients.
+//  At this time we should use mutex to avoid race condition.
+//  Goroutine does not solve all problems and we still have to fix all race conditions manually.
+
+func (s *TcpChatServer) accept(conn net.Conn) *client {
+	log.Printf("Accepting connection from %v, total clients: %v", conn.RemoteAddr().String(), len(s.clients)+1)
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+	client := &client{
+		conn:   conn,
+		writer: protocol.NewCommandWriter(conn),
+	}
+	s.clients = append(s.clients, client)
+	return client
+}
+func (s *TcpChatServer) remove(client *client) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+	// remove the connections from clients array
+	for i, check := range s.clients {
+		if check == client {
+			s.clients = append(s.clients[:i], s.clients[i+1:]...)
+		}
+	}
+	log.Printf("Closing connection from %v", client.conn.RemoteAddr().String())
+	client.conn.Close()
+}
+
+// The main method of serve is to read the message from the client and manage each message accordingly.
+//  Since we have two protocol read and write methods,
+//   the server only needs to interact with the messages at a high level and there is no need to communicate with the byte stream.
+//  If the server receives a SendCommand, all it does is broadcast the message to all other clients.
+
+func (s *TcpChatServer) serve(client *client) {
+	cmdReader := protocol.NewCommandReader(client.conn)
+	defer s.remove(client)
+	for {
+		cmd, err := cmdReader.Read()
+		if err != nil && err != io.EOF {
+			log.Printf("Read error: %v", err)
+		}
+		if cmd != nil {
+			switch v := cmd.(type) {
+			case protocol.SendCommand:
+				go s.Broadcast(protocol.MessageCommand{
+					Message: v.Message,
+					Name:    client.name,
+				})
+			case protocol.NameCommand:
+				client.name = v.Name
+			}
+		}
+		if err == io.EOF {
+			break
+		}
+	}
+}
+
+func (s *TcpChatServer) Broadcast(command interface{}) error {
+	for _, client := range s.clients {
+
+		client.writer.Write(command)
+	}
+	return nil
+}
--- readme.md
+++ readme.md
@@ -29,3 +29,47 @@ s you can see, the handler sends a “Hello!” message as we pass this response
 Now let’s try out this setup. Start the web server with go run server.go and visit http://localhost:8080/hello. If the server responds with "Hello!", you can continue to the next step, where you’ll learn how to add basic security to your Golang web server routes.
 
 it was ok
+
+Add basic security to routes
+It goes without saying that security is important. Let’s explore some basic strategies to enhance the security of your Go web server.
+
+Before we do, we should take a moment to increase the readability of our code. Let’s create the helloHandler function, which holds all the logic related to the /hello request.
+
+
+This handler uses the Request object to check whether the requested path is correct. This is a very basic example of how you can use the Request object.
+
+If the path is incorrect, the server returns a StatusNotFound error to the user. To write an error to the user, you can use the http.Error method. Notice that the StatusNotFound code corresponds to a 404 error. All status codes can be found in the Golang documentation.
+
+Next, we add a check for verifying the type of the request. If the method doesn’t correspond to GET, the server returns a new error. When both checks pass, the server returns its success response "Hello!".
+
+The last thing we need to do is modify the handleFunc function in our main() function to accept the above helloHandler function.
+
+
+Next, we’ll start the Go web server with go run server.go. You can test your security by sending a POST request to http://localhost:8080/hello using a tool such as Postman or cURL.
+
+Start a static web server
+In this step, we’ll create a simple file server to host static files. This will be a very simple addition to the web server.
+
+To make sure we have content to serve on the web server, let’s modify the index.html file located in the static folder. To keep things simple, just add a heading to the file that says “Static Website.” If you wish, you can add more files or styling files to make your web server look a bit nicer.
+
+To serve the static folder, you’ll have to add two lines of code to server.go. The first line of code creates the file server object using the FileServer function. This function accepts a path in the http.Dir type. Therefore, we have to convert the string path “./static” to an http.Dir path type.
+
+Don’t forget to specify the Handle route, which accepts a path and the fileserver. This function acts in the same way as the HandleFunc function, with some small differences.
+
+It’s time to try out the code. Fire up the server with go run server.go and visit http://localhost:8080/. You should see the “Static Website” header.
+
+Accept a form submission POST request
+Lastly, the web server has to respond to a form submission.
+
+Let’s add some content to the form.html file in the static folder. Notice that the form action is sent to /form. This means the POST request from the form will be sent to http://localhost:8080/form. The form itself asks for input for two variables: name and address.
+
+The next step is to create the handler to accept the /form request. The form.html file is already served via the FileServer and can be accessed via http://localhost:8080/form.html.
+
+First, the function has to call ParseForm() to parse the raw query and update r.PostForm and r.Form. This will allow us to access the name and address values via the r.FormValue method.
+
+At the end of the function, we write both values to the ResponseWriter using fmt.Fprintf.
+
+Trying out the form handler
+We can test the form by starting the server with go run server.go. When the server starts, visit http://localhost:8080/form.html. You should see two input fields and a submit button.
+
+When you’ve filled out the form, hit the submit button. The server should process your POST request and show you the result on the http://localhost:8080/form response page.
